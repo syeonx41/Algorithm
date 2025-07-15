5fa74abf036b179aa77468de76a494fb5ccb317c
SeungyeonWoo <seungyeon3287@gmail.com>
2025-07-15 20:21:04 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,16 @@
+c=input()
+c=int(c)
+
+if c>=90 :
+    print("A")
+else :
+    if c>=80 :
+        print("B")
+    else :
+        if c>=70 :
+            print("C")
+        else :
+            if c>=60 :
+                print("D")
+            else :
+                print("F")
