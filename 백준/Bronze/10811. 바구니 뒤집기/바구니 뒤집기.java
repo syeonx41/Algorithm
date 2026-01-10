c8429a7d9710f7344e96d58ef00abcaf5fd44a4a
SEUNGYEON WOO <seungyeon3287@gmail.com>
2026-01-10 15:20:19 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 124 ms, Memory: 15880 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] basket = new int[n+1];
+        for (int i=1; i<n+1; i++){
+            basket[i] = i;
+        }
+
+        for (int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            while(a<b) {
+                int temp = 0;
+                temp = basket[a];
+                basket[a] = basket[b];
+                basket[b] = temp;
+                a++;
+                b--;
+                }
+            }
+
+        for (int i=1; i<n+1; i++){
+            System.out.print(basket[i] + " ");
+        }
+    }
+}
